7d22eb6953043a047abd1bbd6055532bed124eeb
Alex Xu (Hello71) <alex_y_xu@yahoo.ca>
2018-07-20 18:40:23 -0400

    add minimal setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='dbinterfacer',
+    version='0.0.1',
+    url='https://github.com/csb-comren/dbinterfacer',
+
+    description='Abstraction layer between the database and django',
+)
